0371fce9cbd58e40368f29660abcd53c874d1c5b
Leroy B <shoked@gmail.com>
2025-11-04 08:30:39 -0500

    so far

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,79 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+
+import { Header } from "./components/Header.jsx";
+import { Footer } from "./components/Footer.jsx";
+import { Pagination } from "./components/Pagination.jsx";
+import { SearchFormSection } from "./components/SearchFormSection.jsx";
+import { JobListings } from "./components/JobListings.jsx";
+
+import jobsData from "./data.json";
+
+const RESULTS_PER_PAGE = 4;
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [filters, setFilters] = useState({
+    technology: "",
+    location: "",
+    experienceLevel: "",
+  });
+  const [textToFilter, setTextToFilter] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const jobsFilteredByFilters = jobsData.filter((job) => {
+    return (
+      filters.technology === "" || job.data.technology === filters.technology
+    );
+  });
+
+  const jobsWithTextFilter =
+    textToFilter === ""
+      ? jobsFilteredByFilters
+      : jobsFilteredByFilters.filter((job) => {
+          return job.titulo.toLowerCase().includes(textToFilter.toLowerCase());
+        });
+
+  const totalPages = Math.ceil(jobsWithTextFilter.length / RESULTS_PER_PAGE);
+
+  const pagedResults = jobsWithTextFilter.slice(
+    (currentPage - 1) * RESULTS_PER_PAGE, // Página 1 -> 0, Página 2 -> 5, Página 3 -> 10
+    currentPage * RESULTS_PER_PAGE // Página 1 -> 5, Página 2 -> 10, Página 3 -> 15
+  );
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  const handleSearch = (filters) => {
+    setFilters(filters);
+    setCurrentPage(1);
+  };
+
+  const handleTextFilter = (newTextToFilter) => {
+    setTextToFilter(newTextToFilter);
+    setCurrentPage(1);
+  };
 
   return (
     <>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
+      <Header />
+
+      <main>
+        <SearchFormSection
+          onSearch={handleSearch}
+          onTextFilter={handleTextFilter}
+        />
+
+        <section>
+          <JobListings jobs={pagedResults} />
+          <Pagination
+            currentPage={currentPage}
+            totalPages={totalPages}
+            onPageChange={handlePageChange}
+          />
+        </section>
+      </main>
+
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <small>&copy; 2025 DevJobs. Todos los derechos reservados.</small>
+    </footer>
+  );
+};
+
+export default Footer;
